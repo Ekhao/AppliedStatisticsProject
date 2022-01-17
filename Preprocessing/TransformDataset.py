ca4a4f016deccf18d69c833affca93bc53bad56b
Emil Njor <Emil.Njor@gmail.com>
2022-01-17 14:26:28 +0100

    Cleaned up preprocessing program

--- Preprocessing/TransformDataset.py
+++ Preprocessing/TransformDataset.py
@@ -1,9 +1,8 @@
 # This python program was used to convert the original dataset into a properly formatted dataset with categories as columns and observations as rows
 
-import numpy as np
 import pandas as pd
 
-PdM_Predictions = pd.read_csv("Data.csv", header=None)
+PdM_Predictions = pd.read_csv("Original_Data.csv", header=None)
 
 PdM_Predictions = PdM_Predictions.T
 PdM_Predictions = PdM_Predictions.rename(
