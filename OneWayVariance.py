286df4e0325f8ef4df792a22849fcb71f7f87580
Emil Njor <Emil.Njor@gmail.com>
2022-01-18 11:15:59 +0100

    made a boxplot in a very annoying way

--- OneWayVariance.py
+++ OneWayVariance.py
@@ -5,12 +5,15 @@
 from operator import index
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # First the data is loaded
 RUL_Pred = pd.read_csv("Data.csv")
 
 # The data in its current form is not appropriate for a one way analysis of variance
 # It needs to be transformed to having a categorical explanitory variable
+# TODO: Make this dataframe be able to take different amounts of actual RULs and predictions.
 RUL_Pred = pd.DataFrame(
     {
         "Index": range(300),
@@ -31,3 +34,14 @@ print(RUL_Pred["Source"].value_counts())
 
 
 # We then do a few plots of the data
+# A boxplot would do nicely
+boxplot = RUL_Pred.boxplot(
+    by="Source", patch_artist=True, grid=False)
+boxplot.set_ylabel("Remaining Useful Life (RUL)")
+boxplot.set_xlabel("Model")
+boxplot.set_title("Remaining Useful Life ground truth and predictions")
+plt.savefig("Figures/OneWayBoxPlot.png")
+# Maybe a stripchart
+
+
+# Then we conduct a one way analysis of variance
